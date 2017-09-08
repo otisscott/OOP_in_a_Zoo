a1ca30e9bb3cda7d051f6c9d1a9fc86291e9b74c
Otis Scott <otismscott@gmail.com>
2017-09-08 09:39:19 -0700

    Finished Project

--- src/Runner.java
+++ src/Runner.java
@@ -1,19 +1,18 @@
+import java.util.ArrayList;
 public class Runner {
-
     public static void main(String[] args) {
         Tiger tigger = new Tiger("Tigger");
-        tigger.eat("meat");
-        tigger.eat("kibble");
         Bear pooh = new Bear("Pooh");
-        pooh.eat("fish");
-        pooh.eat("meat");
         Unicorn rarity = new Unicorn("Rarity");
-        rarity.eat("marshmallows");
         Giraffe gemma = new Giraffe("Gemma");
-        gemma.eat("meat");
-        gemma.eat("leaves");
         Bee stinger = new Bee("Stinger");
-        stinger.eat("ice cream");
-        stinger.eat("pollen");
+        Zookeeper zoebot = new Zookeeper("Zoebot");
+        ArrayList<Animal> animals = new ArrayList<>();
+        animals.add(tigger);
+        animals.add(pooh);
+        animals.add(rarity);
+        animals.add(gemma);
+        animals.add(stinger);
+        zoebot.feedAnimals(animals,"pizza");
     }
 }
